06da47ac04eea12eda7f268dcba8dea2a954e5ac
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-08-08 13:38:20 +0530

    not loading full screen bug fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {HttpInterceptor} from "../providers/http-interceptor.service";
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,22 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private activatedRoute:ActivatedRoute) { }
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    HomeComponent.loadAdminLTEScripts();
+  }
 
   ngOnInit() {
-    this.router.navigate(['explorer'], { relativeTo: this.activatedRoute });
+    this.router.navigate(['explorer'], {relativeTo: this.activatedRoute});
+  }
+
+  static loadAdminLTEScripts() {
+    let node = document.createElement('script');
+    node.src = "../src/js/app.min.js";
+    node.type = 'text/javascript';
+    node.async = true;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 
 }
