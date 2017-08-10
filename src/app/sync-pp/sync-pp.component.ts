468e1a6ea50ca12fecde7b25503249446ab650c6
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-08-10 12:11:45 +0530

    pd-pd ui completed

--- src/app/sync-pp/sync-pp.component.ts
+++ src/app/sync-pp/sync-pp.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {PocketDrive} from "../models/pocketdrive";
+
+/*const LINKS = [
+  [{name: 'Office PD', uuid: '1001', ip: '192.168.8.100', port: '3000'}, {name: 'Home PD', uuid: '1002', ip: '248.112.30.12', port: '3000'}],
+  [{name: 'Floor 1', uuid: '1101', ip: '192.168.1.1', port: '3000'}, {name: 'Floor 2', uuid: '1001', ip: '192.168.1.10', port: '3000'}]
+];*/
+
+const od = {name: 'Office PD', uuid: '1001', ip: '192.168.8.100', port: '3000'};
 
 @Component({
   selector: 'app-sync-pp',
@@ -7,7 +15,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SyncPdPdComponent implements OnInit {
 
-  constructor() { }
+  /*links: PocketDrive[][];
+  p: PocketDrive*/
+
+  constructor() {
+    // this.links = LINKS;
+  }
 
   ngOnInit() {
   }
